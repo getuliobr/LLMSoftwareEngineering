7fe0c7008391364ad1dd183aeb67e11236640d49
getuliobr <getulioregis@alunos.utfpr.edu.br>
2025-10-08 14:36:25 -0300

    feat(agent): create an agent to handle tool calls properly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from reader import SCHEMA_SQL
 from langchain_ollama import ChatOllama
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import InMemorySaver
 from tools import *
 
 llm = ChatOllama(
@@ -8,54 +10,22 @@ llm = ChatOllama(
     num_ctx=128000
 )
 
-tools = {
-    'sql_query_executor': sql_query_executor,
-    'get_user_info': get_user_info,
-    'web_search': web_search,
-    'visit_url': visit_url,
-    'get_repository_issue_info': get_repository_issue_info,
-}
+agent = create_react_agent(
+    llm,
+    tools=[
+        sql_query_executor,
+        get_user_info,
+        web_search,
+        visit_url,
+        get_repository_issue_info,
+    ],
+    checkpointer=InMemorySaver()
+)
 
-llm_with_tools = llm.bind_tools([ tool for tool in tools.values() ])
-
-history = [
-    ("system", f"You are a helpful assistant that helps people find information in a database of GitHub issues. Use the tools to answer questions. The database schema is as follows:\n```sql\n{SCHEMA_SQL}```"),
-]
-
+config = {"configurable": {"thread_id": "1"}}
 while msg := input("Enter your question (or 'exit' to quit): "):
-    
     if msg.lower() == 'exit':
         break
-    history.append(("user", msg))
-    history.append(llm_with_tools.invoke(history))
-
-    reasoning = history[-1].additional_kwargs.get('reasoning_content', '')
-    if reasoning:
-        reasoning = f"<think>{reasoning}</think>"
-
-    print("Assistant:", reasoning)
-
-    while len(history[-1].tool_calls) > 0:
-        response = history[-1]
-        print(response)
-        tool_call_count = 0
-        for tool_call in response.tool_calls:
-            tool_name = tool_call["name"].lower()
-            selected_tool = tools.get(tool_name)
-
-            if not selected_tool:
-                print(f"Tool {tool_name} not found. ({tool_call})")
-                continue
-            
-            for i in range(5):
-                try:
-                    tool_msg = selected_tool.invoke(tool_call)
-                    history.append(tool_msg)
-                    tool_call_count += 1
-                    break
-                except Exception as e:
-                    print(f"Error invoking tool {tool_name} (attempt {i+1}/3): {e}")
-
-        history.append(llm_with_tools.invoke(history))
-    history.append(llm.invoke(history + [("user", f"Provide a final answer based on the information gathered.\nDo not use any tools.\nThe user asked: {msg}")]))
-    print(history[-1].content)
+    response = agent.invoke({'messages': [{'role': 'user', 'content': msg}]}, config=config)
+    ai_response = response['messages'][-1].content
+    print("Assistant:", ai_response)
